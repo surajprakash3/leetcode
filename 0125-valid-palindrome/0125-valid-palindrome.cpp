8a6e0ebddede621427c8e587c49c3ca821e23331
Suraj  Prakash <141227462+surajprakash3@users.noreply.github.com>
2026-05-01 11:00:10 +0530

    Time: 6 ms (6.67%), Space: 10.8 MB (7.7%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,26 +1,20 @@
+
+
+
 class Solution {
 public:
     bool isPalindrome(string s) {
-string a ="";
-for(int i=0;i<s.size(); i++){
-    if(s[i]>='A' && s[i]<='Z'){
-        int a1 = s[i]+32;
-        char temp=a1;
-        a+=temp;
-    }else if((s[i]>='a' && s[i]<='z')||(s[i]>='0' && s[i]<='9')){
-        a+=s[i];
-            }else continue;
-    }
-    cout<<a;
-    int i = 0;
-    int j = a.size()-1;
-    while (i<j)
-    {
-        if(a[i]!=a[j]) return false;
+        string cleaned = "";
 
-        i++ , j--;
+        for (char c : s) {
+            if (isalnum(c)) {
+                cleaned += tolower(c);
+            }
+        }
+
+        string rev = cleaned;
+        reverse(rev.begin(), rev.end());
+
+        return cleaned == rev;
     }
-    return true;
- }
-    
 };
