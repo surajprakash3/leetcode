ac8424e473678dabcc100f5180092945a7a16913
Suraj  Prakash <141227462+surajprakash3@users.noreply.github.com>
2026-08-30 22:18:13 +0530

    Time: 0 ms (100%), Space: 23.9 MB (19.54%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -3,10 +3,11 @@ public:
     void moveZeroes(vector<int>& nums) {
         int j=0;
         for(int i=0;i<nums.size();i++){
+            
             if(nums[i]!=0){
                 swap(nums[i],nums[j]);
                 j++;
-               
+
             }
         }
         
